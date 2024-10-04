d9fe705a7016e750674cc4aa3313dfccb4abbfe4
paalmijs <palmijskruts@gmail.com>
2024-10-04 15:58:51 +0300

    Konfektes un Zimuļi

---
+++ konfektesunzimuli/konfektes.py
@@ -0,0 +1,10 @@
+mikus = 42
+maris = 70
+
+konfcenax4 = maris - mikus
+konfcena = konfcenax4 / 4
+
+mikusKonf = 42 / konfcena
+mikusKonfR = round(mikusKonf)
+
+print("Vienas konfektes cena ir", konfcena, "un Mikus nopirka", mikusKonfR, "konfektes.")
---
+++ konfektesunzimuli/zimuli.py
@@ -0,0 +1,8 @@
+starpiba = 15 - 12
+
+starpibasCena = 21
+
+zimulaCena = starpibasCena / starpiba
+pNauda = zimulaCena * 12 + 16
+
+print("Viena zīmuļa cena ir", zimulaCena, "centi un pircējam bija", pNauda, "centi.")
